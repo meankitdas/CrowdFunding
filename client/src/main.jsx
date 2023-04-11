24f583048381e33d53565241a36df6afbf2a0fda
Ankit Das <75267634+meankitdas@users.noreply.github.com>
2023-04-11 13:28:50 +0530

    FEAT: Client code added

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,16 +1,20 @@
 import React from "react";
 import ReactDom from "react-dom/client";
-import {BrowserRouter as Router} from "react-router-dom";
-import {ChainId, ThirdwebProvider} from "@thirdweb-dev/react"
+import { BrowserRouter as Router } from "react-router-dom";
+import { ChainId, ThirdwebProvider } from "@thirdweb-dev/react";
 
 import App from "./app";
+import "./index.css";
 
-const root = ReactDom.createRoot(document.getElementById("root"))
+const root = ReactDom.createRoot(document.getElementById("root"));
 
 root.render(
-    <ThirdwebProvider desiredChainId={ChainId.Goerli}>
-        <Router>
-            <App />
-        </Router>
-    </ThirdwebProvider>
-)
+  <ThirdwebProvider desiredChainId={ChainId.Goerli}>
+    <Router>
+      <App />
+      {/* <div>
+        <h1 className="text-yellow-500 text-8xl">Hwwlo</h1>
+      </div> */}
+    </Router>
+  </ThirdwebProvider>
+);
